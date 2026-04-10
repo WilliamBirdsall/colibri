2ac9432fc8b1d8b721fc28d4774ccaba6b0a7c07
Will Birdsall <birdsallwill@protonmail.com>
2026-04-10 09:45:35 -0400

    schemea update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_10_131959) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_10_134207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,11 +60,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_10_131959) do
   end
 
   create_table "events", force: :cascade do |t|
+    t.bigint "book_club_id", null: false
     t.datetime "created_at", null: false
     t.datetime "end"
     t.datetime "start"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.index ["book_club_id"], name: "index_events_on_book_club_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -86,5 +88,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_10_131959) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "events", "book_clubs"
   add_foreign_key "sessions", "users"
 end
